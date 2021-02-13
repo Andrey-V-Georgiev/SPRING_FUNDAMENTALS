4fbc3fc7a5fa1daa2c3d2117f65a44171198d517
Andrey-V-Georgiev <andrey.v.georgiev@gmail.com>
2021-02-13 20:20:01 +0200

    categories seed in better way

--- ShopingList/src/main/java/softuni/shopping_list/services/impl/CategoryServiceImpl.java
+++ ShopingList/src/main/java/softuni/shopping_list/services/impl/CategoryServiceImpl.java
@@ -10,6 +10,7 @@ import softuni.shopping_list.repositories.CategoryRepository;
 import softuni.shopping_list.services.CategoryService;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -25,21 +26,11 @@ public class CategoryServiceImpl implements CategoryService {
 
     @PostConstruct
     public void seedCategories() {
-        System.out.println("HIT PRE");
-        System.out.println(categoryRepository.count());
-
         if (categoryRepository.count() == 0) {
-            /* Create all instances */
-            Category shirt = new Category(CategoryEnum.DRINK);
-            Category denim = new Category(CategoryEnum.FOOD);
-            Category shorts = new Category(CategoryEnum.HOUSEHOLD);
-            Category jacket = new Category(CategoryEnum.OTHER);
-
-            /* Save them to DB*/
-            this.categoryRepository.saveAndFlush(shirt);
-            this.categoryRepository.saveAndFlush(denim);
-            this.categoryRepository.saveAndFlush(shorts);
-            this.categoryRepository.saveAndFlush(jacket);
+            Arrays.stream(CategoryEnum.values()).forEach(categoryName -> {
+                Category category = new Category(categoryName, String.format("Description for %s", categoryName));
+                this.categoryRepository.saveAndFlush(category);
+            });
         }
     }
 
@@ -47,7 +38,7 @@ public class CategoryServiceImpl implements CategoryService {
     public CategoryServiceModel findCategoryByName(CategoryEnum name) {
 
         CategoryServiceModel categoryServiceModel = this.categoryRepository
-                .findCategoryByName(name )
+                .findCategoryByName(name)
                 .map(c -> this.modelMapper.map(c, CategoryServiceModel.class))
                 .orElse(null);
         return categoryServiceModel;

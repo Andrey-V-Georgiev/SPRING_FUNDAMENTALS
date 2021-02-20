777da938f3968f073415d7fa4de7397dea926583
Andrey-V-Georgiev <andrey.v.georgiev@gmail.com>
2021-02-20 11:37:02 +0200

    added dependecy: spring-session-core

--- ShopingList/src/main/java/softuni/shopping_list/repositories/ProductRepository.java
+++ ShopingList/src/main/java/softuni/shopping_list/repositories/ProductRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import softuni.shopping_list.enumerations.CategoryEnum;
-import softuni.shopping_list.models.entity.Category;
 import softuni.shopping_list.models.entity.Product;
 
 import java.math.BigDecimal;
